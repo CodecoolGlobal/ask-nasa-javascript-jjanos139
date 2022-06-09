1a95edaa7e4256898642266f0f7a965afad63491
jjanos139 <jdale139@gmail.com>
2022-06-09 10:16:41 +0200

    Edit the entire code to include useState and useEffect

--- ask-nasa/src/App.js
+++ ask-nasa/src/App.js
@@ -1,70 +1,47 @@
 import './App.css';
+import React from "react"
 
 let urlbase = 'https://api.nasa.gov/planetary/apod?api_key=C85tS7kq4xLTxW1Uwe8PAYF9c6rziAiL42cOLslb&date=';
 let now = new Date();
 let basedate = now.toISOString().slice(0, 10)
-let url = urlbase + basedate;
 
-function App() {
-  let parent = document.getElementById("parent");
-  let img = document.createElement("img");
-  let iframe = document.createElement("iframe");
-  let title = document.createElement("h3");
-  let date = document.createElement("p");
-  let explanation = document.createElement("p");
-  let copyright = document.createElement("p");
+export default function App() {
   let input = document.getElementById("input");
-  function getData() {
+  const [url, setUrl] = React.useState(urlbase + basedate)
+  const [nasaData, setNasaData] = React.useState({})
+
+  React.useEffect(function getData() {
     fetch(url)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        displayItems(data);
+        setNasaData(data)
       })
       .catch(function (error) {
         console.log(error);
       })
-  }
-
-  function displayItems(data) {
-    parent.innerHTML = "";
-    title.innerHTML = data.title;
-    date.innerHTML = data.date;
-    parent.appendChild(title);
-    parent.appendChild(date);
-    if (data.media_type === "image") {
-      img.setAttribute("src", data.url);
-      parent.appendChild(img);
-    }
-    else if (data.media_type === "video") {
-      iframe.setAttribute("src", data.url);
-      parent.appendChild(iframe);
-    }
-    explanation.innerHTML = data.explanation;
-    if (data["copyright"])
-      copyright.innerHTML = "Credited by: " + data.copyright;
-    else
-      copyright.innerHTML = "";
-    parent.appendChild(copyright);
-    parent.appendChild(explanation);
-  }
-
-  getData();
+  }, [url])
 
   function dateChange() {
-    url = urlbase + input.value;
-    App();
+    console.log(input.value)
+    console.log(urlbase)
+    setUrl(urlbase + input.value)
+    console.log(url)
   }
 
   return (
     <div className="App">
       <h1>Image of the day by NASA</h1>
-      <input id="input" type="date" value={basedate} onChange={dateChange}></input>
+      <input id="input" type="date" onChange={dateChange}></input>
       <div id="parent">
+        <h3>{nasaData.title}</h3>
+        <p>{nasaData.date}</p>
+        {nasaData.media_type === "video" ? <iframe src={nasaData.url} title={nasaData.url} /> : ""}
+        {nasaData.media_type === "image" ? <img src={nasaData.url} alt="" /> : ""}
+        <p className='explanation'>{nasaData.explanation}</p>
+        {nasaData.copyright ? <p>Credited by: {nasaData.copyright}</p> : ""}
       </div>
     </div>
   )
 }
-
-export default App;
